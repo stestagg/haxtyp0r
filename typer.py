58a2966fd0e66898e9b92bcd62ec543996c011c1
TomV <tom@viner.tv>
2014-09-04 20:39:40 +0100

    demo mode

--- typer.py
+++ typer.py
@@ -2,21 +2,21 @@ from readkey import getch
 from inter import send_keystroke
 
 
-def demo1(DEBUG):
+def main(DEBUG):
     keygen = getch(0.1)
     charge = 0
     inc = 0.1
     while 1:
         the_input = keygen.next()
         if the_input:
-            # print the_input
             charge = inc + (1 - inc) * charge
             tempo = int(charge  *100)
             if DEBUG:
                 print tempo * 'X'
-            send_keystroke(tempo)
+            else:
+                send_keystroke(tempo)
         else:
             charge = (1 - inc) * charge
 
 if __name__ == '__main__':
-    demo1(DEBUG=False)
+    main(DEBUG=False)
