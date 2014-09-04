1722fdd263c6a7c642f385927d780103c26e8470
Steve Stagg <stestagg@Steves-MacBook-Pro.local>
2014-09-04 20:34:55 +0100

    aaa

--- inter.py
+++ inter.py
@@ -2,17 +2,40 @@
 import os
 import sys
 import random
-location = os.listdir("source")
-myfile = random.sample(location, 1)[0]
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import Terminal256Formatter
 
-with open("source/"+myfile) as thefile:
-	content = thefile.read()
+from pygments.lexers import guess_lexer, guess_lexer_for_filename
 
+formatted = None
 progress = 0
+
+def load_random_file():
+	global formatted, progress
+
+	files = os.listdir("source")
+	myfile = random.sample(files, 1)[0]
+	with open("source/"+myfile) as thefile:
+		content = thefile.read()
+
+	lexer = guess_lexer_for_filename(myfile, content)
+	formatted = highlight(content,lexer,Terminal256Formatter())
+	progress = 0
+
+load_random_file()
 	 
 def send_keystroke(tempo):
 	global progress
-	sys.stdout.write(content[progress:progress+tempo])
+	if tempo == 0:
+		return
+	next_chars = formatted[progress:progress+tempo]
+	if len(next_chars) == 0:
+		# end of file reached
+		load_random_file()
+		return send_keystroke(tempo)
+
+	sys.stdout.write(next_chars)
 	sys.stdout.flush()
 	progress = progress + tempo
 
