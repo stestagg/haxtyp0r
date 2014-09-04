7e9b6009522f098cba6b02d6c2da0dc28995d27e
TomV <tom@viner.tv>
2014-09-04 20:22:31 +0100

    connect up to send_keystroke

--- typer.py
+++ typer.py
@@ -2,18 +2,21 @@ from readkey import getch
 from inter import send_keystroke
 
 
-def demo1():
+def demo1(DEBUG):
     keygen = getch(0.1)
-    tempo = 0
+    charge = 0
     inc = 0.1
     while 1:
         the_input = keygen.next()
         if the_input:
             # print the_input
-            tempo = inc + (1 - inc) * tempo
-            print int(tempo*100) * 'X'
+            charge = inc + (1 - inc) * charge
+            tempo = int(charge  *100)
+            if DEBUG:
+                print tempo * 'X'
+            send_keystroke(tempo)
         else:
-            tempo = (1 - inc) * tempo
+            charge = (1 - inc) * charge
 
 if __name__ == '__main__':
-    demo1()
+    demo1(DEBUG=False)
