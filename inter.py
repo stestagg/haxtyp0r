6161610f15f5b1f610ba8b1fcc2fe3ce7d71f993
Steve Stagg <stestagg@Steves-MacBook-Pro.local>
2014-09-04 20:15:30 +0100

    Inital commit

--- inter.py
+++ inter.py
@@ -1,3 +1,26 @@
 
-def send_keystroke(char):
-	return "some data"
+import os
+import sys
+import random
+location = os.listdir("source")
+myfile = random.sample(location, 1)[0]
+
+with open("source/"+myfile) as thefile:
+	content = thefile.read()
+
+progress = 0
+	 
+def send_keystroke(tempo):
+	global progress
+	sys.stdout.write(content[progress:progress+tempo])
+	sys.stdout.flush()
+	progress = progress + tempo
+
+def main():
+	while True:
+		raw_input()
+		send_keystroke(random.randint(1, 10))
+
+
+if __name__ == '__main__':
+	main()
